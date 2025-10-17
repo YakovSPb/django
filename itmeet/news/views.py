2d304a2322bc4f47fffa49330fec2cce2e15ead6
Yakov <itmeetm@gmail.com>
2025-10-17 15:02:42 +0300

    lesson 8

--- itmeet/news/views.py
+++ itmeet/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Artiles
 
 # Create your views here.
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Artiles.objects.order_by('-data')
+    return render(request, 'news/news_home.html', {'news':news})
